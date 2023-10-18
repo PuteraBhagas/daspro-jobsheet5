07a3a98db8f3b2b875e200216be3299350335ddb
PuteraBhagas <p.bhagaswara@gmail.com>
2023-10-18 22:41:01 +0700

    jobsheet ke 5

---
+++ PemilihanPeercobaan225.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * PemilihanPeercobaan225
+ */
+public class PemilihanPeercobaan225 {
+
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+        System.out.println("Nilai uas   : ");
+        float uas = input25.nextFloat();
+        System.out.println("Nilai uts   : ");
+        float uts = input25.nextFloat();
+        System.out.println("Nilai kuis  : ");
+        float kuis = input25.nextFloat();
+        System.out.println("Nilai tugas : ");
+        float tugas = input25.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+
+    }
+}
---
+++ PemilihanPercobaan125.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * PemilihanPercobaan125
+ */
+public class PemilihanPercobaan125 {
+
+    public static void main(String[] args) {
+        Scanner input25= new Scanner(System.in);
+        System.out.print("Masukan angka:  ");
+        int angka = input25.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka "+angka+" bilangan genap");
+            
+        } else {
+            System.out.println("Angka "+angka+" bilangan ganjil");
+            
+        }
+    }
+}
